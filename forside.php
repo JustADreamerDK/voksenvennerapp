c9738edfabbf584f47c982f0fd5d41e3353a5c11
Katja Kjer <Kalle3008@hotmail.com>
2018-12-04 12:59:59 +0100

    forsiden, minimenu virker

--- forside.php
+++ forside.php
@@ -11,6 +11,9 @@ $venskabsId = $row['venskab_id'];
 $day = getDayById($venskabsId);
 $dayTest = getDayById($venskabsId);
 $rowDayTest = mysqli_fetch_assoc($dayTest);
+
+$start = 0;
+$limit = 2;
 ?>
 <!DOCTYPE html>
 <html lang="da">
@@ -25,103 +28,134 @@ $rowDayTest = mysqli_fetch_assoc($dayTest);
     <?php
     include "include/header.php";
     ?>
-    <section class="flex-column center">
-        <?php if ($rowDayTest['id'] <> '' && $type == '2' OR $type =='3'){ ?>
-            <?php while ($rowDay = mysqli_fetch_assoc($day)) {
+    <section class="flex-column center p-b-175">
+        <?php if ($rowDayTest['id'] == '' && $type == '2' OR $type =='3'){
                 ?>
-                <div class="special w-80 m-t-50 p-25">
-                    <div class="flex between w-100">
-                        <div>
-                            <h3 class="bold"><?php echo $rowDay['overskrift']; ?></h3>
-                        </div>
-                        <div class="flex">
-                            <h3><?php echo $rowDay['dato']; ?></h3>
-                            <h3 class="p-l-30" id="aaben">⋮</h3>
-                        </div>
-                    </div>
-                    <div class="flex-column right">
-                        <ul class="minimenu">
-                            <li><a href=""><h3>Download</h3></a></li>
-                            <li><a href="rediger-dag.php?id=<?php echo $rowDay['id']; ?>"><h3>Rediger</h3></a></li>
-                            <li><a href="slet-dag.php?id=<?php echo $rowDay['id']; ?>"><h3>Slet</h3></a></li>
-                        </ul>
-                    </div>
-                    <?php $dagId = $rowDay['id'];
-                    $billederne = getPictures($dagId);
+                <h2 class="m-t-half">Tilføj en dag</h2>
+                <img class="m-t-50" src="img/head.png">
+            <?php } ?>
+            <?php if ($type == 2 || $type == 3) {
+                ?>
+                <a href="tilfoej-dag.php"><button class="tilfoej">+</button></a>
+            <?php }if($type == '1'){
+                $days = getDays();
+                while ($rowDays = mysqli_fetch_assoc($days)) {
                     ?>
-                    <?php
-                    // while (
-                        $rowPic = mysqli_fetch_assoc($billederne);
-                    // ) {
-                        ?>
-                        <img class="w-100 m-t-10 " src="img/<?php echo $rowPic['filnavn'] ?>">
-                        <?php
-                    // }
-                    ?>
-                    <!-- <div class="button">
-                        <button class="slidebutton" onclick="plusDivs(-1)">&#10094;</button>
-                        <button class="slidebutton" onclick="plusDivs(1)">&#10095;</button>
-                    </div> -->
-                    <?php
-                    $beskrivelse = mb_substr($rowDay['beskrivelse'], 0, 120, 'UTF-8');
-                    $tal = mb_strrpos($beskrivelse, ' ', 0, 'UTF-8');
-                    $beskrivelse = mb_substr($rowDay['beskrivelse'], 0, $tal, 'UTF-8');
-                    ?>
-                    <p><?php echo $beskrivelse . '...' ?></p>
-                    <h4 class="m-t-10">
-                        <a href="read-more.php?id=<?php echo $rowDay['id']; ?>">Læs mere<a>
-                        </h4>
-                    </div>
-                    <?php
-                }
-            }if ($rowDayTest['id'] == '' && $type == '2' OR $type =='3'){
-                        ?>
-                        <h2 class="m-t-half">Tilføj en dag</h2>
-                        <img class="m-t-50" src="img/head.png">
-                    <?php } ?>
-                    <?php if ($type == 2 || $type == 3) {
-                        ?>
-                        <a href="tilfoej-dag.php"><button class="tilfoej">+</button></a>
-                    <?php }if($type == '1'){
-                        $days = getDays();
-                        while ($rowDays = mysqli_fetch_assoc($days)) {
-                            ?>
-                            <div class="special w-80 m-t-50 p-25">
-                                <div class="flex between w-100">
-                                    <div>
-                                        <h3 class="bold"><?php echo $rowDays['overskrift']; ?></h3>
-                                    </div>
-                                    <div class="flex">
-                                        <h3><?php echo $rowDays['dato']; ?></h3>
-                                    </div>
-                                </div>
-                                <?php $dagsId = $rowDays['id'];
-                                $billedernes = getPictures($dagsId);
-                                ?>
-                                <?php while ($rowPics = mysqli_fetch_assoc($billedernes)) {
+                    <div class="special w-80 m-t-50 p-25">
+                        <div class="flex between w-100">
+                            <div>
+                                <h3 class="bold"><?php echo $rowDays['overskrift']; ?></h3>
+                            </div>
+                            <div class="flex">
+                                <h3>
+                                    <?php $date = $rowDays['dato'];
+                                    $dato = new DateTime("$date");
+                                    echo $dato->format('d-m-Y');
                                     ?>
-                                    <img class="w-100 m-t-10 slide fade" src="img/<?php echo $rowPics['filnavn'] ?>">
-                                    <?php
-                                }
-                                ?>
-                                <div class="button">
-                                    <button class="slidebutton" onclick="plusDivs(-1)">&#10094;</button>
-                                    <button class="slidebutton" onclick="plusDivs(1)">&#10095;</button>
-                                </div>
+                                </h3>
+                            </div>
+                        </div>
+                        <?php $dagsId = $rowDays['id'];
+                        $billedernes = getPictures($dagsId);
+                        ?>
+                        <?php
+                        $rowPics = mysqli_fetch_assoc($billedernes);
+                        ?>
+                        <img class="w-100 m-t-10 " src="img/<?php echo $rowPics['filnavn'] ?>">
 
-                                <?php
-                                $beskrivelse = mb_substr($rowDays['beskrivelse'], 0, 120, 'UTF-8');
-                                $tal = mb_strrpos($beskrivelse, ' ', 0, 'UTF-8');
-                                $beskrivelse = mb_substr($rowDays['beskrivelse'], 0, $tal, 'UTF-8');
-                                ?>
-                                <p><?php echo $beskrivelse . '...' ?></p>
-                                <h4 class="m-t-10">
-                                    <a href="read-more.php?id=<?php echo $rowDays['id']; ?>">Læs mere<a>
-                                    </h4>
-                                </div>
-                                <?php
-                            }}
-?>
+                        <?php
+                        $beskrivelse = mb_substr($rowDays['beskrivelse'], 0, 120, 'UTF-8');
+                        $tal = mb_strrpos($beskrivelse, ' ', 0, 'UTF-8');
+                        $beskrivelse = mb_substr($rowDays['beskrivelse'], 0, $tal, 'UTF-8');
+                        ?>
+                        <p><?php echo $beskrivelse . '...' ?></p>
+                        <h4 class="m-t-10">
+                            <a href="read-more.php?id=<?php echo $rowDays['id']; ?>">Læs mere<a>
+                            </h4>
+                        </div>
+                        <?php
+                    }}
+                    ?>
+
+                    <?php
+        if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+            $start = ($page - 1) * $limit;
+        } else {
+            $page = 1;
+        }
+        ?>
+        <?php if ($rowDayTest['id'] <> '' && $type == '2' OR $type =='3'){ ?>
+<?php
+        $day = mysqli_query($objCon, "SELECT * FROM `dag` WHERE `venskabs_id` = '$venskabsId' LIMIT $start, $limit");
+        while ($rowDay = mysqli_fetch_assoc($day)) {
+            $i++;
+            ?>
+            <div class="special w-80 m-t-50 p-25">
+                <div class="flex between w-100">
+                    <div>
+                        <h3 class="bold"><?php echo $rowDay['overskrift']; ?></h3>
+                    </div>
+                    <div class="flex">
+                        <h3>
+                            <?php $date = $rowDay['dato'];
+                            $dato = new DateTime("$date");
+                            echo $dato->format('d-m-Y');
+                            ?>
+                        </h3>
+                        <h3 class="p-l-30" id="aaben<?php echo $i; ?>">⋮</h3>
+                    </div>
+                </div>
+                <div class="flex-column right">
+                    <ul class="minimenu<?php echo $i; ?>">
+                        <li><a href="rediger-dag.php?id=<?php echo $rowDay['id']; ?>"><h3>Rediger</h3></a></li>
+                        <li><a href="slet-dag.php?id=<?php echo $rowDay['id']; ?>"><h3>Slet</h3></a></li>
+                    </ul>
+                </div>
+                <?php $dagId = $rowDay['id'];
+                $billederne = getPictures($dagId);
+                ?>
+                <?php
+                $rowPic = mysqli_fetch_assoc($billederne);
+                ?>
+                <img class="w-100 m-t-10 " src="img/<?php echo $rowPic['filnavn'] ?>">
+                <?php
+                $beskrivelse = mb_substr($rowDay['beskrivelse'], 0, 120, 'UTF-8');
+                $tal = mb_strrpos($beskrivelse, ' ', 0, 'UTF-8');
+                $beskrivelse = mb_substr($rowDay['beskrivelse'], 0, $tal, 'UTF-8');
+                ?>
+                <p><?php echo $beskrivelse . '...' ?></p>
+                <h4 class="m-t-10">
+                    <a href="read-more.php?id=<?php echo $rowDay['id']; ?>">Læs mere<a>
+                    </h4>
+                </div>
+        <?php
+    };
+ ?>
+
+        <div class="flex m-t-10 between w-80">
+            <?php
+            $rows = mysqli_num_rows(mysqli_query($objCon, "SELECT * FROM `dag`"));
+            $total = ceil($rows / $limit);
+            if ($page > 1) {
+                echo "<a href='?page=" . ($page - 1) . "'><h3 class='bladre'>Forrige</h3></a>";
+            } else {
+                echo "<div></div>";
+            }
+            ?>
+
+            <?php
+            if ($page != $total) {
+                echo "<a href='?page=" . ($page + 1) . "'><h3 class='bladre'>Næste</h3></a>";
+            } else {
+                echo "<div></div>";
+            }}
+            ?>
+
+
+
+
+
                 </section>
                 <script src="javascript/script.js"></script>
             </body>
--- javascript/script.js
+++ javascript/script.js
@@ -17,16 +17,50 @@ closeMenuBtn.addEventListener("click", function(event){
 });
 
 // Minimenu
-var aabenMinimenu = document.querySelector(".minimenu");
+var aabenMinimenu1 = document.querySelector(".minimenu1");
+var showMinimenuBtn1 = document.querySelector("#aaben1");
 
-var showMinimenuBtn = document.querySelector("#aaben");
-
-if(aabenMinimenu){
-showMinimenuBtn.addEventListener("click", function(event){
-    aabenMinimenu.style.display = "block";
+if(aabenMinimenu1){
+showMinimenuBtn1.addEventListener("click", function(event){
+    aabenMinimenu1.style.display = "block";
 })};
 
+var aabenMinimenu2 = document.querySelector(".minimenu2");
+var showMinimenuBtn2 = document.querySelector("#aaben2");
+
+if(aabenMinimenu2){
+showMinimenuBtn2.addEventListener("click", function(event){
+    aabenMinimenu2.style.display = "block";
+})};
+
+var aabenMinimenu3 = document.querySelector(".minimenu3");
+var showMinimenuBtn3 = document.querySelector("#aaben3");
+
+if(aabenMinimenu3){
+showMinimenuBtn3.addEventListener("click", function(event){
+    aabenMinimenu3.style.display = "block";
+})};
+
+var aabenMinimenu4 = document.querySelector(".minimenu4");
+var showMinimenuBtn4 = document.querySelector("#aaben4");
+
+if(aabenMinimenu4){
+showMinimenuBtn4.addEventListener("click", function(event){
+    aabenMinimenu4.style.display = "block";
+})};
+
+var aabenMinimenu5 = document.querySelector(".minimenu5");
+var showMinimenuBtn5 = document.querySelector("#aaben5");
+
+if(aabenMinimenu5){
+showMinimenuBtn5.addEventListener("click", function(event){
+    aabenMinimenu5.style.display = "block";
+})};
+
+
 // Slideshow
+var slide = document.querySelector(".slidebutton");
+if(slide){
 var slideIndex = 1;
     showDivs(slideIndex);
 
@@ -44,3 +78,4 @@ var slideIndex = 1;
       }
       x[slideIndex-1].style.display = "block";
     }
+};
